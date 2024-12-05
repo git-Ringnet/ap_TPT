66eddeea59ece926583c21e06d18b17aca9ac027
Thái Minh Hiếu <87421130+HieuTamihi@users.noreply.github.com>
2024-12-05 08:50:54 +0700

    data group type

--- database/migrations/2024_12_04_095638_create_group_types_table.php
+++ database/migrations/2024_12_04_095638_create_group_types_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -16,6 +17,30 @@ return new class extends Migration
             $table->string('group_name')->nullable();
             $table->timestamps();
         });
+        
+        //import data group_types
+        DB::table('group_types')->insert([
+            [
+                'id' => 1,
+                'group_name' => 'Khách hàng',
+            ],
+            [
+                'id' => 2,
+                'group_name' => 'Nhà cung cấp',
+            ],
+            [
+                'id' => 3,
+                'group_name' => 'Hàng hóa',
+            ],
+            [
+                'id' => 4,
+                'group_name' => 'Nhân viên',
+            ],
+            [
+                'id' => 5,
+                'group_name' => 'Kho',
+            ],
+        ]);
     }
 
     /**
