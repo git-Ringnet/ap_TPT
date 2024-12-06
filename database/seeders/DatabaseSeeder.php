c4ac213eddbc5bd0dde6a2beff4f240bccc21582
Nguyễn Quốc Việt <88937231+nqv1710@users.noreply.github.com>
2024-12-06 08:49:55 +0700

    push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,6 +20,6 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-        // $this->call(PermissionSeeder::class);
+        $this->call(RoleSeeder::class);
     }
 }
