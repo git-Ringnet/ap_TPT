67d7fbc1f6419b260c404e34ef48e8f6d2cee4b0
Nguyễn Quốc Việt <88937231+nqv1710@users.noreply.github.com>
2025-02-14 00:38:55 +0700

    change user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,22 +20,22 @@ class UserSeeder extends Seeder
         // Tạo người dùng mặc định
         $admin = User::create([
             'name' => 'Admin User',
-            'email' => 'admin@ringnet.vn',
-            'password' => Hash::make('Ringnet@123'),
+            'email' => 'admin@thienphattien.com',
+            'password' => Hash::make('Admin@123'),
         ]);
         $admin->assignRole('Admin');
 
         $warehouseManager = User::create([
             'name' => 'Quản lý kho',
-            'email' => 'quankho@ringnet.vn',
-            'password' => Hash::make('Ringnet@123'),
+            'email' => 'quankho@thienphattien.com',
+            'password' => Hash::make('Quankho@123'),
         ]);
         $warehouseManager->assignRole('Quản lý kho');
 
         $serviceUser = User::create([
             'name' => 'Bảo hành',
-            'email' => 'baohanh@ringnet.vn',
-            'password' => Hash::make('Ringnet@123'),
+            'email' => 'baohanh@thienphattien.com',
+            'password' => Hash::make('Baohanh@123'),
         ]);
         $serviceUser->assignRole('Bảo hành');
 
